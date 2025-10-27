b7965604e4ca79b971b615b203a0a17c4c88cd86
mdmontezuma <maruti.davari@montezuma.ai>
2025-10-27 11:15:10 +0530

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pspmandal/',
+  // base: '/pspmandal/',
   build: {
     chunkSizeWarningLimit: 1000, // You can adjust the limit as needed
   },
